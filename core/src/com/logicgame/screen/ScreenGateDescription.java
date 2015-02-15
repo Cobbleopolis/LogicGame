a9930d48cc6f61ebc8ed4fd2db117cdfdcb39937
Cobbleopolis <cobbleopolis@gmail.com>
2015-02-15 09:20:41 -0500

    Finished ScreenGateDescription

--- core/src/com/logicgame/screen/ScreenGateDescription.java
+++ core/src/com/logicgame/screen/ScreenGateDescription.java
@@ -23,6 +23,8 @@ public class ScreenGateDescription implements Screen {
     Texture bridgeGate = new Texture("bridge1.png");
     Texture andGate = new Texture("and_on.png");
     Texture nandGate = new Texture("nand_on.png");
+    Texture xorGate = new Texture("xor_on.png");
+
 
     BitmapFont font;
 
@@ -58,20 +60,27 @@ public class ScreenGateDescription implements Screen {
 
         spriteBatch.draw(wireTex, 10, Gdx.graphics.getHeight() - 200, 100, 100);
         font.drawMultiLine(
-                spriteBatch, "Carries signal.", 130, Gdx.graphics.getHeight() - (200 - (float) (font.getBounds("Carries signal").height * 1.5)));
+                spriteBatch, "Wire - carries signal.", 130, Gdx.graphics.getHeight() - (200 - (float) (font.getBounds("Carries signal").height * 1.5)));
 
         spriteBatch.draw(notGate, 10, Gdx.graphics.getHeight() - 380, 100, 100);
         font.drawMultiLine(
-                spriteBatch, "Inverts signal.", 130, Gdx.graphics.getHeight() - (350 - (float)(font.getBounds("Inverts Signal").height * 1.5)));
+                spriteBatch, "NOT Gate - Inverts signal.", 130, Gdx.graphics.getHeight() - (350 - (float)(font.getBounds(" ").height * 1.5)));
 
         spriteBatch.draw(bridgeGate, 10, Gdx.graphics.getHeight() - 560, 100, 100);
         font.drawMultiLine(
-                spriteBatch, "Allows two lines to intersect\nwithout affecting the signals.", 130, Gdx.graphics.getHeight() - (520 - (float)(font.getBounds("Inverts Signal").height * 1.5)));
+                spriteBatch, "Bridge - Allows two lines to\nintersect without affecting the signals.", 130, Gdx.graphics.getHeight() - (520 - (float)(font.getBounds(" ").height * 1.5)));
 
         spriteBatch.draw(andGate, 10, Gdx.graphics.getHeight() - 740, 100, 100);
         font.drawMultiLine(
-                spriteBatch, "Both inputs must be on for\nits output to be on.", 130, Gdx.graphics.getHeight() - (700 - (float)(font.getBounds("Inverts Signal").height * 1.5)));
+                spriteBatch, "AND Gate - Both inputs must\nbe on for its output to be on.", 130, Gdx.graphics.getHeight() - (700 - (float)(font.getBounds(" ").height * 1.5)));
 
+        spriteBatch.draw(nandGate, 10, Gdx.graphics.getHeight() - 920, 100, 100);
+        font.drawMultiLine(
+                spriteBatch, "NAND Gate - Both inputs must\nbe off for its output to be on.", 130, Gdx.graphics.getHeight() - (880 - (float)(font.getBounds(" ").height * 1.5)));
+
+        spriteBatch.draw(xorGate, 10, Gdx.graphics.getHeight() - 1100, 100, 100);
+        font.drawMultiLine(
+                spriteBatch, "XOR Gate - For the output to be\non, either input can be on, but\nnot both.", 130, Gdx.graphics.getHeight() - (1060 - (float)(font.getBounds(" ").height * 1.5)));
         spriteBatch.end();
         if (Gdx.input.isKeyPressed(Input.Keys.BACK) && LogicGame.backDelay == 0){
             game.setScreen(new ScreenMainMenu(game));
